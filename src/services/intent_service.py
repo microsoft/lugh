f2627ac49b961c75ca873bb855b7c194965e1178
skraymo1 <kyleraymond@microsoft.com>
2025-01-24 08:49:38 -0600

    updated path read for the plugin

--- src/services/intent_service.py
+++ src/services/intent_service.py
@@ -5,10 +5,9 @@ from semantic_kernel.functions.kernel_arguments import KernelArguments
 
 class IntentService:
     def __init__(self):
-        pass
+        self.pluginsloc = os.path.abspath(os.path.join(os.getcwd(), "plugins"))
 
     async def determine_intent(self, kernel: Kernel, query):
-        pluginsloc = os.path.join(os.getcwd(), "src", "plugins")
-        category_plugin=kernel.add_plugin(parent_directory=pluginsloc, plugin_name="CategoryPlugin")        
+        category_plugin=kernel.add_plugin(parent_directory=self.pluginsloc, plugin_name="CategoryPlugin")        
         category_intent = await kernel.invoke(category_plugin["CategoryDetector"], KernelArguments(input=query))
         return category_intent.value[0].content
